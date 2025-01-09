b677d342e12872880f61d541c4b55ca90c086328
Abdurrahman <arahmanmh15@gmail.com>
2025-01-09 22:26:46 +0800

    02 create job project + model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+    //php 7.4+
+    public static array $experiences = ['entry', 'intermediate', 'senior'];
+    public static array $categories = ['IT', 'Finance', 'Marketing', 'Sales'];
 }
